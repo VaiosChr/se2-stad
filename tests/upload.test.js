906d25bc22a3f294ccd58658dc8f1263bc285a84
Vaios Christodoulou <vallos1501@gmail.com>
2023-12-11 12:50:41 +0200

    initialized testing files

---
+++ tests/upload.test.js
@@ -0,0 +1,6 @@
+/*
+1. POST /admin/class/{className}/user/{userName}/tests
+2. POST /class/{className}/user/{userName}/notes
+3. POST /class/{className}/user/{userName}/exercises 
+4. PUT /admin/user/{userName}/role
+*/
