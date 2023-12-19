849df31c52870ff5f35d465ad2f4c68dbe1fccd2
Vaios Christodoulou <vallos1501@gmail.com>
2023-12-19 12:00:56 +0200

    tests and notes testing

--- tests/upload.test.js
+++ tests/upload.test.js
@@ -25,6 +25,61 @@ test.after.always((t) => {
 });
 
 
+// POST /admin/class/{className}/user/{userName}/tests
+test('POST /admin/class/{className}/user/{userName}/tests', async (t) => {
+    const { statusCode } = await t.context.got.post('admin/class/{className}/user/{userName}/tests', {
+        json: {
+            title: "string",
+            file: "string",
+            id: 0
+        }
+    })
+
+    t.is(statusCode, 200, 'Expected status code 200 for successful post')
+});
+
+// adminClassClassNameUserUserNameTestsPOST function
+test('POST /admin/class/{className}/user/{userName}/tests by adminClassClassNameUserUserNameTestsPOST function', async (t) => {
+    const className = 'className_dummy';
+    const userName = 'userName_dummy';
+    const tests = {
+        title: "string",
+        file: "string",
+        id: 0
+    }
+
+    await t.notThrowsAsync(async () => {
+        await onlyFunc.adminClassClassNameUserUserNameTestsPOST(className, userName, tests);
+    });
+});
+
+//POST /class/{className}/user/{userName}/notes
+test('POST /class/{className}/user/{userName}/notes', async (t) => {
+    const { statusCode } = await t.context.got.post('class/{className}/user/{userName}/notes', {
+        json: {
+            title: "string",
+            file: "string"
+        }
+    })
+
+    t.is(statusCode, 200, 'Expected status code 200 for successful post')
+});
+
+// classClassNameUserUserNameNotesPOST function
+test('POST /class/{className}/user/{userName}/notes by classClassNameUserUserNameNotesPOST function', async (t) => {
+    const className = 'className_dummy';
+    const userName = 'userName_dummy';
+    const notes = {
+        title: "string",
+        file: "string"
+    };
+
+    await t.notThrowsAsync(async () => {
+        await onlyFunc.classClassNameUserUserNameNotesPOST(className, userName, notes);
+    });
+});
+
+
 test('PUT admin/user/{userName}/role', async (t) => {
     const updatedUser = {
         nameRole:'Teacher',
